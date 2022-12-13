887d5644584c42dae9962430410a0d7fe258ed67
schaefer.w@telekom.de <schaefer.w@telekom.de>
2022-12-13 10:39:45 +0100

    feat:common add mult as function to arrays

--- src/common/utils/arrayExtension.ts
+++ src/common/utils/arrayExtension.ts
@@ -2,6 +2,7 @@ export { }
 declare global {
     interface Array<T> {
         sum(exp?: (item: T, index: number) => number): number;
+        mult(exp?: (item: T, index: number) => number): number;
     }
 }
 
@@ -12,6 +13,19 @@ if (!Array.prototype.sum) {
             result = this.map(exp).reduce((sum, it) => sum + it, 0)
         } else {
             result = this.reduce((sum, it) => sum + it, 0)
+        }
+
+        return result;
+    };
+}
+
+if (!Array.prototype.mult) {
+    Array.prototype.mult = function <T>(exp?: (item: T, index: number) => number): number {
+        var result: number = 0;
+        if (exp) {
+            result = this.map(exp).reduce((prod, it) => prod * it, 1)
+        } else {
+            result = this.reduce((prod, it) => prod * it, 1)
         }
 
         return result;
--- src/day00/solution1.ts
+++ src/day00/solution1.ts
@@ -1,11 +1,8 @@
 import { Solver } from "../common/utils/solver"
 import "../common/utils/arrayExtension"
-import _ from 'lodash'
-import dijkstraAlgorithm from "../common/algorithms/dijkstra"
 
 const solution: any = (input: string) => {
     const entries = input.split("\n")
-    console.log(_(entries).sumBy(it => it.length))
     console.log([1, 2, 3].sum())
     return entries
 }
